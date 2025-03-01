f798d5ae6a002c9980ffbe80ec2ae1f00f85438d
Alexandre Silva <84932904+alexnmsilva@users.noreply.github.com>
2025-03-01 18:51:39 +0000

    Alinhamento de código em Program

--- Program.cs
+++ Program.cs
@@ -9,20 +9,20 @@ namespace Calculos
 	
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Aplicação Cálculos");
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Aplicação Cálculos");
 			
 			Console.WriteLine($"4 + 2 ={Aritmetica.Somar(4, 2)}");
 			Console.WriteLine($"4 - 2 ={Aritmetica.Subtrair(4, 2)}");
 			
 			double t1 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.CelsiusFahrenheit, 36);
-            double t2 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.FahrenheitCelsius, 100);
+			double t2 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.FahrenheitCelsius, 100);
 
-            Console.WriteLine($"36º Celsius = {t1}º Fahrenheit");
-            Console.WriteLine($"100º Fahrenheit = {t2}º Celsius");
+			Console.WriteLine($"36º Celsius = {t1}º Fahrenheit");
+			Console.WriteLine($"100º Fahrenheit = {t2}º Celsius");
 			
-            Console.ReadKey();
+			Console.ReadKey();
         }
     }
 }
