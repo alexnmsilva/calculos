83935cb6ef7bd39e9106be8e198a246824604005
Alexandre Silva <84932904+alexnmsilva@users.noreply.github.com>
2025-03-01 18:36:09 +0000

    Criar a classe Conversões e mover método
    O Método ConverterTemperatura() foi removido da classe Aritmetica e colocada na classe Converoes uma vez que implementa uma operação que não se adequa à class.

--- Program.cs
+++ Program.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 
 namespace Calculos
 {
+	
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Aplicação Cálculos");
-
+			
+			Console.WriteLine($"4 + 2 ={Aritmetica.Somar(4, 2)}");
+			Console.WriteLine($"4 - 2 ={Aritmetica.Subtrair(4, 2)}");
+			
             Console.ReadKey();
         }
     }
